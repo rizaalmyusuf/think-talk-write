901b7320cfd24cbee1279cb0560ec036a5de809c
Rizal M. Yusuf <rijalmy010600@gmail.com>
2024-07-11 10:43:03 +0700

    user Model

--- app/Config/Security.php
+++ app/Config/Security.php
@@ -15,7 +15,7 @@ class Security extends BaseConfig
      *
      * @var string 'cookie' or 'session'
      */
-    public string $csrfProtection = 'cookie';
+    public string $csrfProtection = 'session';
 
     /**
      * --------------------------------------------------------------------------
@@ -83,6 +83,7 @@ class Security extends BaseConfig
      * @see https://codeigniter4.github.io/userguide/libraries/security.html#redirection-on-failure
      */
     public bool $redirect = (ENVIRONMENT === 'production');
+    // public bool $redirect = true;
 
     /**
      * --------------------------------------------------------------------------
